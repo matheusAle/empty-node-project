be81e5236957f1a4272053c70e280e2afc078bec
Matheus Ale da Silva <m.matheus.ale@gmail.com>
2019-11-04 23:55:16 +0000

    Solicitando dados de varias fontes diferentes para formar uma tabela

--- prj/src/app/app.component.ts
+++ prj/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Http }       from '@angular/http';
-
 import 'rxjs/add/operator/toPromise';
+
+const baseApi = 'https://3000-bfba4571-8d46-46e0-b0bf-4d24f9b9aeea.ws-us02.gitpod.io';
 
 @Component({
   selector: 'app-root',
@@ -10,37 +11,67 @@ import 'rxjs/add/operator/toPromise';
 })
 export class AppComponent {
 
-  user = {
-    name: '',
-    email: ''
-  };
+  products = [];
+  file = null;
 
-  result = {}
-
-  usersList = [];
+  form = {
+    product: '',
+    description: '',
+    fileId: undefined,
+  }
 
   constructor(private http: Http) {
-    this.listUsers();
+    this.listProducts();
   }
 
-  registerUser() {
-    this.http.post(
-      'https://3000-bfba4571-8d46-46e0-b0bf-4d24f9b9aeea.ws-us1.gitpod.io/users',
-      this.user
-    )
-    .toPromise()
-    .then((data) => {
-        this.result = data;
-    })
+
+  async listProducts() {
+    const productsList = await this.getProducts();
+
+    for (let product of productsList) {
+      product.price = await this.getPriceOfProduct(product);
+      product.description = await this.getDescriptionOfProduct(product);
+    }
+
+    this.products = productsList;
+
   }
 
-  listUsers() {
-    this.http.get(
-      'https://3000-bfba4571-8d46-46e0-b0bf-4d24f9b9aeea.ws-us1.gitpod.io/users',
-    )
-    .toPromise()
-    .then((data) => {
-      this.usersList = data.json();
-    })
+  async getProducts() {
+    return (await this.http.get(`${baseApi}/products`).toPromise()).json();
+  }
+
+  async getPriceOfProduct(product) {
+    const price = (await this.http.get(`${baseApi}/prices?productId=${product.id}`).toPromise()).json();
+
+    return price[0]
+  }
+
+  async getDescriptionOfProduct(product) {
+    const desc = (await this.http.get(`${baseApi}/descriptions?productId=${product.id}`).toPromise()).json();
+
+    return desc[0]
+  }
+
+  async submitForm() {
+
+    // upload do arquivo
+    //  -> salva os dados dos campos com o id do arquivo no storage.
+
+    if (this.file !== null) {
+      this.form.fileId = await this.uploadFile(this.file);
+      this.saveProduct(this.form);
+
+    } else {
+      this.saveProduct(this.form);
+    }
+  }
+
+  async uploadFile(file) {
+
+  }
+
+  async saveProduct(product) {
+
   }
 }
